9451a1872a9bd1bc8f60dc30967d268a6de924b0
siddharth195 <siddharthmalhotra147@gmail.com>
2024-06-25 23:50:21 +0530

    updating

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button>Home</button>
+      <button>My network ()</button>
+      <button>Jobs ()</button>
+      <button>Messaging()</button>
+      <button>Notification</button>
+      <button>Me</button>
+
+
+
+    </>
+  )
+}
+
+export default App
