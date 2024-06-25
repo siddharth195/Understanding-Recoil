f02f945cb0e2a4c6a6b88c00cbedca8af6a82c13
siddharth195 <siddharthmalhotra147@gmail.com>
2024-06-26 00:29:46 +0530

    Added Recoil

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom, totalNotifcationSelector } from './atoms'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
 
   return (
-    <>
-      <button>Home</button>
-      <button>My network ()</button>
-      <button>Jobs ()</button>
-      <button>Messaging()</button>
-      <button>Notification</button>
-      <button>Me</button>
-
-
-
-    </>
+    <RecoilRoot>
+    <MainApp></MainApp>
+    </RecoilRoot>
   )
 }
 
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const jobsAtomCount = useRecoilValue(jobsAtom)
+  const notificationAtomCount = useRecoilValue(notificationAtom)
+  const [messagingAtomCount,setMessagingAtomCount] = useRecoilState(messagingAtom)
+  const totalNotificationCount = useRecoilValue(totalNotifcationSelector)
+
+  return <>
+
+        <button>Home</button>
+        <button>My network ({networkNotificationCount > 100 ? "99+": networkNotificationCount})</button>
+        <button>Jobs ({jobsAtomCount})</button>
+        <button>Messaging({messagingAtomCount})</button>
+        <button>Notification ({notificationAtomCount})</button>
+        <button>Me ({totalNotificationCount})</button>
+
+  </>
+}
 export default App
